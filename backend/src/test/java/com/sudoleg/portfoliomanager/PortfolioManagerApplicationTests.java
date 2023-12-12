730628da435f426dbfe91a673edb18928f193f0a
sudoleg <oleg.hansemerkur@gmail.com>
2023-12-12 22:18:57 +0100

    initialize backend using spring initializr

---
+++ backend/src/test/java/com/sudoleg/portfoliomanager/PortfolioManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sudoleg.portfoliomanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
