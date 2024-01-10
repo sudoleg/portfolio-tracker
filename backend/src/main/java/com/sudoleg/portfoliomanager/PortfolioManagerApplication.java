4af38d74e892579c8715b94e8011b3c862bb462c
sudoleg <oleg.hansemerkur@gmail.com>
2024-01-10 13:54:22 +0100

    feat: connect to postgres db

--- backend/src/main/java/com/sudoleg/portfoliomanager/PortfolioManagerApplication.java
+++ backend/src/main/java/com/sudoleg/portfoliomanager/PortfolioManagerApplication.java
@@ -2,12 +2,31 @@ package com.sudoleg.portfoliomanager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import lombok.extern.java.Log;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication
-public class PortfolioManagerApplication {
+@Log
+public class PortfolioManagerApplication implements CommandLineRunner {
+
+	private final DataSource dataSource;
+
+	public PortfolioManagerApplication(final DataSource dataSource) {
+		this.dataSource = dataSource;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(PortfolioManagerApplication.class, args);
+	}
+
+	@Override
+	public void run(final String... args) {
+		log.info("Datasource: " + dataSource.toString());
+		final JdbcTemplate restTemplate = new JdbcTemplate(dataSource);
+		restTemplate.execute("select 1");
 	}
 
 }
