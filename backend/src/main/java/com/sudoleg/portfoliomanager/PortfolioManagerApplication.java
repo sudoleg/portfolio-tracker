feb884883abf61a13d4e77f99032374f27ffe1c3
sudoleg <oleg.hansemerkur@gmail.com>
2024-01-10 14:43:18 +0100

    refactor: imports

--- backend/src/main/java/com/sudoleg/portfoliomanager/PortfolioManagerApplication.java
+++ backend/src/main/java/com/sudoleg/portfoliomanager/PortfolioManagerApplication.java
@@ -1,9 +1,9 @@
 package com.sudoleg.portfoliomanager;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import lombok.extern.java.Log;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 import javax.sql.DataSource;
