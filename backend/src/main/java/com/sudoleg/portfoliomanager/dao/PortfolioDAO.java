aa196b395a58029bafa7b1839c8e13752e4ce7cb
sudoleg <oleg.hansemerkur@gmail.com>
2024-01-12 21:38:57 +0100

    refactor: add DAOs for users and portfolios

--- backend/src/main/java/com/sudoleg/portfoliomanager/dao/PortfolioDAO.java
+++ backend/src/main/java/com/sudoleg/portfoliomanager/dao/PortfolioDAO.java
@@ -1,4 +1,7 @@
 package com.sudoleg.portfoliomanager.dao;
 
+import com.sudoleg.portfoliomanager.domain.Portfolio;
+
 public interface PortfolioDAO {
+    void create(Portfolio portfolio);
 }
--- backend/src/main/java/com/sudoleg/portfoliomanager/dao/UserDAO.java
+++ backend/src/main/java/com/sudoleg/portfoliomanager/dao/UserDAO.java
@@ -1,4 +1,7 @@
 package com.sudoleg.portfoliomanager.dao;
 
+import com.sudoleg.portfoliomanager.domain.User;
+
 public interface UserDAO {
+    void create(User user);
 }
