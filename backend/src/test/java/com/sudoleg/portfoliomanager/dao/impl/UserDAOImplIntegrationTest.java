191e79a2b3ced08525484b329d265d8ade4f3ca3
sudoleg <oleg.hansemerkur@gmail.com>
2024-01-19 18:11:28 +0100

    test: clean up context after each test method run

--- backend/src/test/java/com/sudoleg/portfoliomanager/dao/impl/UserDAOImplIntegrationTest.java
+++ backend/src/test/java/com/sudoleg/portfoliomanager/dao/impl/UserDAOImplIntegrationTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.List;
@@ -15,6 +16,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 @ExtendWith(SpringExtension.class)
+// ensures that the context is cleaned up after each test
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 public class UserDAOImplIntegrationTest {
 
     private UserDAOImpl underTest;
