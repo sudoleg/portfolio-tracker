5d7f0b862efa1fee54182cbd713548c640ba6b81
sudoleg <oleg.hansemerkur@gmail.com>
2024-01-21 16:13:52 +0100

    refactor: refactor domain classes to entities

--- backend/src/main/java/com/sudoleg/portfoliomanager/domain/Portfolio.java
+++ backend/src/main/java/com/sudoleg/portfoliomanager/domain/Portfolio.java
@@ -1,5 +1,6 @@
 package com.sudoleg.portfoliomanager.domain;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -9,10 +10,20 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
+@Entity
+@Table(name = "portfolios")
 public class Portfolio {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "portfolio_id_seq")
     private Integer portfolioId;
+
     private String name;
-    private Integer userId;
+
+    // If we retrieve a book, we also retrieve the user. And if we make changes
+    // to that user, they get saved to the DB as well.
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "user_id")
+    private User user;
 
 }
---
+++ backend/src/main/java/com/sudoleg/portfoliomanager/repositories/PortfolioRepository.java
@@ -0,0 +1,4 @@
+package com.sudoleg.portfoliomanager.repositories;
+
+public interface PortfolioRepository {
+}
