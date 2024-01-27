c9a58d6b489e4faee89d7b21d2efd0dfbd5dfb69
sudoleg <oleg.hansemerkur@gmail.com>
2024-01-27 12:47:06 +0100

    feat: enable nested objects

--- backend/src/main/java/com/sudoleg/portfoliomanager/config/MapperConfig.java
+++ backend/src/main/java/com/sudoleg/portfoliomanager/config/MapperConfig.java
@@ -1,16 +1,22 @@
 package com.sudoleg.portfoliomanager.config;
 
 import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-
+/**
+ * Mapper configuration.
+ * The mapper is configured to map nested objects.
+ */
 @Configuration
 public class MapperConfig {
 
     @Bean
     public ModelMapper modelMapper() {
-        return new ModelMapper();
+        ModelMapper modelMapper = new ModelMapper();
+        modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.LOOSE);
+        return modelMapper;
     }
 
 }
