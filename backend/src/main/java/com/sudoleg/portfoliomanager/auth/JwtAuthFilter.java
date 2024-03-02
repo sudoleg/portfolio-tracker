e1d44a4d0d454c6f9b553f11d0cea98afb3c8014
Oleg Korznikov <oleg.hansemerkur@gmail.com>
2024-03-02 19:38:45 +0100

    refactor(auth): implement JwtAuthFilter for intercepting requests, validating JWT and setting security context

--- backend/src/main/java/com/sudoleg/portfoliomanager/auth/JwtAuthFilter.java
+++ backend/src/main/java/com/sudoleg/portfoliomanager/auth/JwtAuthFilter.java
@@ -6,38 +6,72 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.lang.NonNull;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
 /**
- * Intercepts every request.
- * Checks JWT.
+ * A filter that intercepts all HTTP requests to validate the JWT token.
+ * It extracts and checks the JWT from the Authorization header of each request.
+ * If the token is valid, it sets the authentication in the security context,
+ * allowing the request to proceed to the controller with authenticated user details.
  */
 @Component
 @RequiredArgsConstructor
 public class JwtAuthFilter extends OncePerRequestFilter {
 
     private final JwtService jwtService;
+    private final UserDetailsService userDetailsService;
 
+    /**
+     * Processes an incoming request to authenticate the user based on the JWT token.
+     * It extracts the JWT token from the Authorization header, validates it,
+     * and sets the user authentication in the security context.
+     *
+     * @param request     the request object containing the HTTP request data.
+     * @param response    the response object containing the HTTP response data.
+     * @param filterChain the filter chain that allows the request to proceed to the next filter or resource.
+     * @throws ServletException if there is an error processing the request.
+     * @throws IOException      if an I/O error occurs during request processing.
+     */
     @Override
     protected void doFilterInternal(
             @NonNull HttpServletRequest request,
             @NonNull HttpServletResponse response,
             @NonNull FilterChain filterChain
     ) throws ServletException, IOException {
-        final String authHeader = request.getHeader("Authorization"); // This header contains the auth token (Bearer).
-        final String jwt;
-        final String username;
+        final String authHeader = request.getHeader("Authorization"); // Extracts the Authorization header.
 
         if (authHeader == null || !authHeader.startsWith("Bearer ")) {
-            filterChain.doFilter(request, response);
+            filterChain.doFilter(request, response); // Proceed without authentication if no valid Authorization header is present.
             return;
         }
 
-        jwt = authHeader.substring(7);
-        username = jwtService.extractUsername(jwt);
+        final String jwt = authHeader.substring(7); // Extracts the JWT token from the header.
+        final String username = jwtService.extractUsername(jwt); // Extracts the username from the JWT.
+
+        // Checks if user details are not already set in the security context.
+        if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+            UserDetails userDetails = this.userDetailsService.loadUserByUsername(username);
+
+            // Validates the token and sets the user authentication in the security context.
+            if (jwtService.isTokenValid(jwt, userDetails)) {
+                UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
+                        userDetails, null, userDetails.getAuthorities());
+
+                authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+            }
+        }
+
+        filterChain.doFilter(request, response); // Proceeds with the filter chain.
     }
 
 }
+
---
+++ backend/src/main/java/com/sudoleg/portfoliomanager/config/ApplicationConfig.java
@@ -0,0 +1,22 @@
+package com.sudoleg.portfoliomanager.config;
+
+import com.sudoleg.portfoliomanager.repositories.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+@Configuration
+@RequiredArgsConstructor
+public class ApplicationConfig {
+
+    private final UserRepository userRepository;
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return username -> userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found!"));
+    }
+
+}
