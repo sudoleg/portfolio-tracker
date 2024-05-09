c97f796193937ecf40de2d988af05a6deb4a2b9f
Oleg Korznikov <oleg.hansemerkur@gmail.com>
2024-05-09 22:01:33 +0200

    docs: define schema for user DTO

--- backend/src/main/java/com/sudoleg/portfoliomanager/domain/dto/UserDto.java
+++ backend/src/main/java/com/sudoleg/portfoliomanager/domain/dto/UserDto.java
@@ -1,5 +1,6 @@
 package com.sudoleg.portfoliomanager.domain.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -10,15 +11,18 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Builder
 public class UserDto {
-
+    @Schema(description = "Automatically generated user ID of the user", example = "1", accessMode = Schema.AccessMode.READ_ONLY)
     private Integer id;
 
+    @Schema(description = "", example = "my-username", accessMode = Schema.AccessMode.READ_WRITE)
     private String username;
 
+    @Schema(description = "First name", example = "John", accessMode = Schema.AccessMode.READ_WRITE)
     private String name;
 
+    @Schema(description = "Family name", example = "Doe", accessMode = Schema.AccessMode.READ_WRITE)
     private String surname;
 
+    @Schema(description = "Email of the user", example = "john.doe@email.com", accessMode = Schema.AccessMode.READ_WRITE)
     private String email;
-
 }
