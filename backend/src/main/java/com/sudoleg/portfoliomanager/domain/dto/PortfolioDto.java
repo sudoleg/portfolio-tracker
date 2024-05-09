0c727775717d8c5e3af0da54a8a9ad81e530e60d
Oleg Korznikov <oleg.hansemerkur@gmail.com>
2024-05-09 20:07:24 +0200

    docs: define schema for portfolio DTO

--- backend/src/main/java/com/sudoleg/portfoliomanager/domain/dto/PortfolioDto.java
+++ backend/src/main/java/com/sudoleg/portfoliomanager/domain/dto/PortfolioDto.java
@@ -1,5 +1,6 @@
 package com.sudoleg.portfoliomanager.domain.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -10,9 +11,12 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Builder
 public class PortfolioDto {
-
+    @Schema(description = "Automatically generated portfolio ID", example = "1", accessMode = Schema.AccessMode.READ_ONLY)
     private Integer portfolioId;
-    private String name;
-    private UserDto user;
 
+    @Schema(description = "Name of the portfolio", example = "My Portfolio", requiredMode = Schema.RequiredMode.REQUIRED)
+    private String name;
+
+    @Schema(description = "User ID of the portfolio owner", example = "1", requiredMode = Schema.RequiredMode.REQUIRED)
+    private Integer userId;
 }
