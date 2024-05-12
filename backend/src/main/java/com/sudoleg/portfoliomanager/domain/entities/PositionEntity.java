08bae00725c2b7fb66219bef778fdf218c2dce28
Oleg Korznikov <oleg.hansemerkur@gmail.com>
2024-05-12 12:55:20 +0200

    refactor: add entity for positions in a portfolio

---
+++ backend/src/main/java/com/sudoleg/portfoliomanager/domain/entities/PositionEntity.java
@@ -0,0 +1,38 @@
+package com.sudoleg.portfoliomanager.domain.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * Represents a holding or position of a financial instrument in a user's investment portfolio.
+ */
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "positions")
+public class PositionEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "position_id_seq")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "portfolio_id", nullable = false)
+    private PortfolioEntity portfolio;
+
+    @Column(nullable = false)
+    private Integer quantity;
+
+    @ManyToOne
+    @JoinColumn(name = "financial_instrument_id", nullable = false)
+    private FinancialInstrument financialInstrument;
+
+    @Column(nullable = false)
+    private double purchasePrice;
+
+}
