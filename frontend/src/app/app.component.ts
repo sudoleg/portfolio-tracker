ec6a6428d7c107fe8edad08bc970fa9fecf96151
Oleg Korznikov <oleg.hansemerkur@gmail.com>
2024-05-18 20:39:59 +0200

    feat: add routing and nav-bar, closes #23

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLinkActive, RouterModule, RouterOutlet } from '@angular/router';
 import { UsersComponent } from './components/users/users.component';
 import { UserDetailsComponent } from './components/user-details/user-details.component';
 import { HttpClientModule } from '@angular/common/http';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, UsersComponent, UserDetailsComponent, HttpClientModule],
+  imports: [RouterOutlet, RouterLinkActive, RouterModule, RouterLink, UsersComponent, UserDetailsComponent, NavBarComponent, HttpClientModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { UsersComponent } from './components/users/users.component';
+import { UserDetailsComponent } from './components/user-details/user-details.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'users', component: UsersComponent, title: "Users list" },
+    { path: 'profile', component: UserDetailsComponent, title: "Profile" }
+];
