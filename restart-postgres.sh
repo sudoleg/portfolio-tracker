4ec1312e7528e8ee5424aeb01e092a4949f4d25b
Oleg Korznikov <oleg.hansemerkur@gmail.com>
2024-06-02 11:11:02 +0200

    chore: reintroduce volume for postgres and adjust restart-postgres script

--- restart-postgres.sh
+++ restart-postgres.sh
@@ -5,10 +5,10 @@ docker stop postgres
 docker rm postgres
 
 # Prune Docker volumes
-docker volume prune -f
+docker volume rm portfolio-tracker_postgres -f
 
 # Start the postgres service in detached mode using docker-compose
 docker-compose up -d postgres
 
 # Output message indicating the script has completed its tasks
-echo "Postgres container stopped, removed, volumes pruned, and postgres service started in detached mode."
+echo "Postgres container stopped, removed, postgres volume deleted, and postgres service started in detached mode."
